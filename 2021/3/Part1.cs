b89a65fdfd4512488d070c8d375fb0340717a82b
Lyx Rothböck <lypsilonx@gmail.com>
2025-12-03 20:06:36 +0100

    2021/3/1

---
+++ 2021/3/Part1.cs
@@ -0,0 +1,44 @@
+using Advent_of_Code.Utility;
+
+namespace Advent_of_Code._2021._3;
+
+public class Part1 : AoCPart
+{
+    public override object Run()
+    {
+        var   inputLines = InputLines();
+        int[] bits       = new int[inputLines[0].Length];
+
+        foreach (var line in inputLines)
+        {
+            for (var index = 0; index < line.Length; index++)
+            {
+                var c = line[index];
+                if (c == '1')
+                {
+                    bits[index]++;
+                }
+            }
+        }
+
+        var gammaBits = bits.Select(x => x > inputLines.Length / 2).ToList();
+        
+        var epsilonBits = gammaBits.Select(x => !x).ToList();
+        var gamma       = BitsToInt(gammaBits);
+        var epsilon       = BitsToInt(epsilonBits);
+
+        return gamma * epsilon;
+    }
+
+    private int BitsToInt(List<bool> bits)
+    {
+        var num = 0;
+        
+        for (var i = 0; i < bits.Count; i++)
+        {
+            num += (bits[i] ? 1 : 0) << bits.Count - i - 1;
+        }
+
+        return num;
+    }
+}
---
+++ 2021/3/Part2.cs
@@ -0,0 +1,11 @@
+using Advent_of_Code.Utility;
+
+namespace Advent_of_Code._2021._3;
+
+public class Part2 : AoCPart
+{
+    public override object Run()
+    {
+        return "";
+    }
+}
