dd69f7e3ca7434e19664083647b7c81212ca25a2
Lyx Rothböck <lypsilonx@gmail.com>
2025-12-03 20:05:45 +0100

    2021/2/2

--- 2021/2/Part2.cs
+++ 2021/2/Part2.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using Advent_of_Code.Utility;
 
 namespace Advent_of_Code._2021._2;
@@ -6,6 +7,29 @@ public class Part2 : AoCPart
 {
     public override object Run()
     {
-        return "";
+        var position = Vector2.Zero;
+        var aim      = 0;
+
+        foreach (var line in InputLines())
+        {
+            var split     = line.Split(" ");
+            var direction = split[0];
+            var amount    = int.Parse(split[1]);
+
+            switch (direction)
+            {
+                case "up":
+                    aim -= amount;
+                    break;
+                case "down":
+                    aim += amount;
+                    break;
+                case "forward":
+                    position += new Vector2(amount, aim * amount);
+                    break;
+            }
+        }
+
+        return (long)position.X * (long)position.Y;
     }
 }
