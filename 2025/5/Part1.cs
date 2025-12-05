a3860f11b380c36deaba67a72a3ae0ba0d4d8aec
Lyx Rothböck <lypsilonx@gmail.com>
2025-12-05 07:16:03 +0100

    2025/5

---
+++ 2025/5/Part1.cs
@@ -0,0 +1,41 @@
+using Advent_of_Code.Utility;
+
+namespace Advent_of_Code._2025._5;
+
+public class Part1 : AoCPart
+{
+    public override string TestInput    => "3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32";
+    public override string TestSolution => "3";
+    
+    public override object Run()
+    {
+        var       fresh    = 0;
+        var       ranges   = true;
+        List<(long, long)> freshIds = [];
+        foreach (var line in InputLines(false))
+        {
+            if (line == "")
+            {
+                ranges = false;
+                continue;
+            }
+
+            if (ranges)
+            {
+                var split = line.Split("-");
+                var start = long.Parse(split[0]);
+                var until = long.Parse(split[1]);
+                freshIds.Add((start, until));
+            }
+            else
+            {
+                var id = long.Parse(line);
+                if (freshIds.Any(x => x.Item1 <= id && x.Item2 >= id))
+                {
+                    fresh++;
+                }
+            }
+        }
+        return fresh;
+    }
+}
---
+++ 2025/5/Part2.cs
@@ -0,0 +1,57 @@
+using Advent_of_Code.Utility;
+
+namespace Advent_of_Code._2025._5;
+
+public class Part2 : AoCPart
+{
+    public override string TestInput    => "11-11\n11-13\n13-15\n11-11\n5-11\n10-11";
+    public override string TestSolution => "11";
+    
+    public override object Run()
+    {
+        long               fresh    = 0;
+        List<(long, long)> freshIds = [];
+        foreach (var line in InputLines(false))
+        {
+            if (line == "")
+            {
+                break;
+            }
+
+            var split = line.Split("-");
+            var start = long.Parse(split[0]);
+            var until = long.Parse(split[1]);
+
+            while (freshIds.Any(x => x.Item1 <= start && x.Item2 >= start))
+            {
+                var startId = freshIds.First(x => x.Item1 <= start && x.Item2 >= start).Item2;
+                start = startId + 1;
+            }
+            
+            while (freshIds.Any(x => x.Item1 <= until && x.Item2 >= until))
+            {
+                var untilId = freshIds.First(x => x.Item1 <= until && x.Item2 >= until).Item1;
+                until = untilId - 1;
+            }
+
+            if (until < start)
+            {
+                continue;
+            }
+            
+            while (freshIds.Any(x => x.Item1 >= start && x.Item2 <= until))
+            {
+                var skipped = freshIds.First(x => x.Item1 >= start && x.Item2 <= until);
+                freshIds.Remove(skipped);
+                var sub = skipped.Item2 + 1 - skipped.Item1;
+                fresh -= sub;
+            }
+            
+            freshIds.Add((start, until));
+            
+            var add = until - start + 1;
+            fresh += add;
+        }
+        return fresh;
+    }
+}
