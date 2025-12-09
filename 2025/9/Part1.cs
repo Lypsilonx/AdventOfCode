fa102202f891a8054b41275d75194eafa48df5f3
Lyx Rothböck <lypsilonx@gmail.com>
2025-12-09 08:04:27 +0100

    2025/9
    This is so stupid O.o

---
+++ 2025/9/Part1.cs
@@ -0,0 +1,47 @@
+using System.Numerics;
+using Advent_of_Code.Utility;
+
+namespace Advent_of_Code._2025._9;
+
+public class Part1 : AoCPart
+{
+    public override List<(string, string)> Tests => [
+        ("""
+         7,1
+         11,1
+         11,7
+         9,7
+         9,5
+         2,5
+         2,3
+         7,3
+         """, "50"),
+    ];
+    
+    public override object Run(string input)
+    {
+        List<Vector2> tiles = [];
+        foreach (var line in SplitInput(input))
+        {
+            var split = line.Split(",");
+            tiles.Add(new Vector2(int.Parse(split[0]), int.Parse(split[1])));
+        }
+
+        long maxArea = 0;
+        foreach (var tileA in tiles)
+        {
+            foreach (var tileB in tiles)
+            {
+                long x    = (long) Math.Abs(tileA.X - tileB.X) + 1;
+                long y    = (long) Math.Abs(tileA.Y - tileB.Y) + 1;
+                long area = x * y;
+                if (area > maxArea)
+                {
+                    maxArea = area;
+                }
+            }
+        }
+        
+        return maxArea;
+    }
+}
---
+++ 2025/9/Part2.cs
@@ -0,0 +1,147 @@
+using System.Numerics;
+using Advent_of_Code.Utility;
+
+namespace Advent_of_Code._2025._9;
+
+public class Part2 : AoCPart
+{
+    public override List<(string, string)> Tests => [
+        ("""
+         7,1
+         11,1
+         11,7
+         9,7
+         9,5
+         2,5
+         2,3
+         7,3
+         """, "24"),
+    ];
+    
+    public override object Run(string input)
+    {
+        List<Vector2> tiles = [];
+        foreach (var line in SplitInput(input))
+        {
+            var split = line.Split(",");
+            tiles.Add(new Vector2(int.Parse(split[0]), int.Parse(split[1])));
+        }
+
+        int    maxX   = (int) tiles.Max(v => v.X) + 2;
+        int    minX   = (int) tiles.Min(v => v.X);
+        int    maxY   = (int) tiles.Max(v => v.Y) + 2;
+        bool[] active = new bool[maxY];
+        bool[][] activeTiles = new bool[maxX][];
+        
+        List<int> lastXTiles = [];
+        for (int x = minX; x < maxX; x++)
+        {
+            while (lastXTiles.Count > 0)
+            {
+                var start = lastXTiles[0];
+                var end   = lastXTiles[1];
+                
+                lastXTiles = lastXTiles.Skip(2).ToList();
+        
+                if (active[start - 1])
+                {
+                    start++;
+                }
+                
+                if (active[end + 1])
+                {
+                    end--;
+                }
+                
+                for (int y = start; y < end + 1; y++)
+                {
+                    active[y] = false;
+                }
+            }
+            
+            var xTiles = tiles.Where(t => t.X == x).OrderBy(t => t.Y).ToList();
+        
+            while (xTiles.Count > 0)
+            {
+                var start = (int) xTiles[0].Y;
+                var end = (int) xTiles[1].Y;
+        
+                xTiles = xTiles.Skip(2).ToList();
+        
+                var filling     = !active[start + 1];
+        
+                if (filling)
+                {
+                    for (int y = start; y < end + 1; y++)
+                    {
+                        active[y] = true;
+                    }
+                }
+                else
+                {
+                    lastXTiles.Add(start);
+                    lastXTiles.Add(end);
+                }
+            }
+            // Console.WriteLine(active.Select(x => x ? "#" : ".").ToList().Join(""));
+            activeTiles[x] = active.Select(x => x).ToArray();
+        }
+        
+        long maxArea = 0;
+        foreach (var tileA in tiles)
+        {
+            foreach (var tileB in tiles)
+            {
+                if (tiles.Any(t => InSquare(tileA, tileB, t)))
+                {
+                    continue;
+                }
+                
+                long x    = (long) Math.Abs(tileA.X - tileB.X) + 1;
+                long y    = (long) Math.Abs(tileA.Y - tileB.Y) + 1;
+                long area = x * y;
+                if (area > maxArea)
+                {
+                    if (!AllActive(tileA, tileB))
+                    {
+                        continue;
+                    }
+                    
+                    maxArea = area;
+                }
+            }
+        }
+        
+        return maxArea;
+
+        bool InSquare(Vector2 a, Vector2 b, Vector2 point)
+        {
+            var xMin = Math.Min(a.X, b.X);
+            var xMax = Math.Max(a.X, b.X);
+            var yMin = Math.Min(a.Y, b.Y);
+            var yMax = Math.Max(a.Y, b.Y);
+            return point.X > xMin && point.X < xMax && point.Y > yMin && point.Y < yMax;
+        }
+
+        bool AllActive(Vector2 a, Vector2 b)
+        {
+            var xMin = (int) Math.Min(a.X, b.X);
+            var xMax = (int) Math.Max(a.X, b.X);
+            var yMin = (int) Math.Min(a.Y, b.Y);
+            var yMax = (int) Math.Max(a.Y, b.Y);
+
+            for (int xI = xMin; xI < xMax + 1; xI++)
+            {
+                for (int yI = yMin; yI < yMax + 1; yI++)
+                {
+                    if (!activeTiles[xI][yI])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
